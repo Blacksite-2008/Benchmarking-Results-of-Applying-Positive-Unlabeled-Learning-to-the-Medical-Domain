82631f276836bceee5b68efbe8d5f4442c29a78f
Blacksite-2008 <53436099+Blacksite-2008@users.noreply.github.com>
2022-06-13 10:31:20 +1000

    Update run.sh

--- prj_imbalanced/run.sh
+++ prj_imbalanced/run.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-#SBATCH --partition=vgpu20
-#SBATCH --gres=gpu:1
-
-srun python3.9 train_without_meta.py -num_initial_pos 300 -learning_rate 7 -weight_decay 8 -num_batches 500 -seed 1 -dataset st
+python train_without_meta.py -num_initial_pos 1000 -learning_rate 4 -weight_decay 8 -num_batches 500 -seed 1 -dataset mit
